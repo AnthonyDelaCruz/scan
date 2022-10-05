d20900f25048a5d3fa805ca6f437497917ba697c
Tony Dela Cruz <delacruztony.adc@gmail.com>
2022-10-05 13:40:54 +0800

    Used next js link component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Links in NextJS app</h1>
-      <a href='/home' style={{ color: 'red' }}>
+      <Link href='/home' style={{ color: 'red' }}>
         Home Page
-      </a>
-      <a href='/about' style={{ color: 'blue' }}>
+      </Link>
+      <Link href='/about' style={{ color: 'blue' }}>
         About Page
-      </a>
-      <a href='/contact' style={{ color: 'yellow' }}>
+      </Link>
+      <Link href='/contact' style={{ color: 'yellow' }}>
         Contact Page
-      </a>
+      </Link>
     </div>
   )
 }
