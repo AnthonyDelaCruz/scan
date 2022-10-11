b40c13c97581171abab0ced126d52a690644aa18
Tony Dela Cruz <delacruztony.adc@gmail.com>
2022-10-11 19:43:53 +0800

    changed color

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
                 <title>Contact</title>
             </Head>
             <h1 style={{ border: "2px solid red", padding: '20px' }}>About</h1>
-            <div style={{ backgroundColor: 'blue', color: 'red' }}>Another DIV</div>
+            <div style={{ backgroundColor: 'blue', color: 'gold' }}>Another DIV</div>
         </React.Fragment>
     )
 }
