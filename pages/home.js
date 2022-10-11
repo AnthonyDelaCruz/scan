8a0d1f506e27de72950a98c63ed6805c382609ba
Tony Dela Cruz <delacruztony.adc@gmail.com>
2022-10-11 17:48:11 +0800

    added color

--- pages/home.js
+++ pages/home.js
@@ -7,7 +7,7 @@ export default function Home() {
             <Head>
                 <title>Home</title>
             </Head>
-            <span style={{ color: 'gold', backgroundColor: 'black' }}>HOME</span>
+            <span style={{ color: 'blue' }}>HOME</span>
         </React.Fragment>
     )
 }
