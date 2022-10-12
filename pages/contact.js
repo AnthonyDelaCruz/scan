9e80910b291277609fd682153e00eec8969d09b5
Tony Dela Cruz <delacruztony.adc@gmail.com>
2022-10-12 13:39:40 +0800

    added styles

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
                 <title>Contact</title>
             </Head>
             <h1 style={{ border: "2px solid red", padding: '20px' }}>About</h1>
-            <div style={{ backgroundColor: 'red', color: 'gold' }}>Another DIV</div>
+            <div style={{ backgroundColor: 'red', color: 'gold', padding: '10px' }}>Another DIV</div>
         </React.Fragment>
     )
 }
