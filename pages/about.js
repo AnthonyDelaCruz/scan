8431ce7dcaa0c99fd9a3897d2e331113adb4997c
Tony Dela Cruz <delacruztony.adc@gmail.com>
2022-10-14 16:36:53 +0800

    updated values

--- pages/about.js
+++ pages/about.js
@@ -3,15 +3,15 @@ import React from 'react'
 
 export default function About() {
     return (
-        <>
+        <div>
             <Head>
                 <title>About</title>
             </Head>
-            <p style={{ fontSize: '40px', color: 'red' }}>About</p>
+            <p style={{ fontSize: '40px', color: 'blue' }}>About</p>
             <a href='https://www.youtube.com/'>Youtube link</a>
             <a href='https://medium.com/'>Twitter link</a>
-            <a href='https://dev.to/'>DEV.to link</a>
-        </>
+            <a href='https://twitter.com'>DEV.to link</a>
+        </div>
     )
 }
 
