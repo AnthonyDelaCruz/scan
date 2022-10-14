4fd221eb816d01d3f5e5c899f08cb9bc350c8a93
Tony Dela Cruz <delacruztony.adc@gmail.com>
2022-10-14 15:36:17 +0800

    changed links

--- pages/about.js
+++ pages/about.js
@@ -3,15 +3,15 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
+        <>
             <Head>
                 <title>About</title>
             </Head>
             <p style={{ fontSize: '40px', color: 'red' }}>About</p>
             <a href='https://www.youtube.com/'>Youtube link</a>
             <a href='https://medium.com/'>Twitter link</a>
-            <a href='https://dev.to/'>DEV.to link</a>
-        </div>
+            <a href='https://twitter.com/'>DEV.to link</a>
+        </>
     )
 }
 
