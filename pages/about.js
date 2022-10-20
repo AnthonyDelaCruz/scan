a5192205691edee87c58289719a2c6b457f65692
Tony Dela Cruz <delacruztony.adc@gmail.com>
2022-10-20 20:36:05 +0800

    removed 1 link added 1 link, changed color of text

--- pages/about.js
+++ pages/about.js
@@ -7,10 +7,10 @@ export default function About() {
             <Head>
                 <title>About</title>
             </Head>
-            <p style={{ fontSize: '40px', color: 'red' }}>About</p>
+            <p style={{ fontSize: '40px', color: 'blue' }}>About</p>
             <a href='https://facebook.com'>fb link</a>
             <a href='https://ant.design'>Ant D link</a>
-            <a href='https://youtube.com'>Youtube</a>
+            <a href='https://dev.to'>Dev.to link</a>
         </div>
     )
 }
