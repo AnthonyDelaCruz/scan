cc924cae14b07d85de3eab7a947983f6d791736c
Tony Dela Cruz <delacruztony.adc@gmail.com>
2022-11-01 17:19:18 +0800

    added buttons

--- pages/contact.js
+++ pages/contact.js
@@ -12,6 +12,11 @@ export default function Contact() {
             <h1 style={{ border: "2px solid red", padding: '20px' }}>Contact</h1>
             <div style={{ backgroundColor: 'blue', color: 'yellow', padding: '20px' }}>Another DIV</div>
             <Button />
+            <br></br>
+            <br></br>
+            <Button />
+            <div>Above last button</div>
+            <Button />
         </React.Fragment>
     )
 }
