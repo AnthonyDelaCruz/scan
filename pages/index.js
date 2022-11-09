a4a1c04f9f9331ce3d3bf362858adf476df79175
Tony Dela Cruz <delacruztony.adc@gmail.com>
2022-11-09 14:42:49 +0800

    added og:image

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
@@ -5,6 +6,12 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <meta
+          property='og:image'
+          content='/zero-reg-og-image.png'
+        />
+      </Head>
       <Link href='/home' >
         <a style={{ color: 'white' }}>
           Home Page
